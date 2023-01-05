fca0567f9a077bffbae97d6310066cebf738fe9d
Rohit Waghole <rohitwaghole4@gmail.com>
2023-01-05 12:46:56 +0530

    Revert "removing game.py"
    This reverts commit 7f10e31be7afc2893b1cb5e9c4142e5f074ec3df.

---
+++ game.py
@@ -0,0 +1,50 @@
+import random
+print('''
+_________________________________________________________________
+|                                                               |
+|    Welcome to the Number Guessing Game!                       |
+|                                                               |
+|    Instructions:                                              |
+|        1. Total 3 lifes given                                 |
+|        2. Select the number range form 0                      |
+|        3. guess the number                                    |
+|        4. if correct ? Hurray, You've won! else You lose!     |
+|_______________________________________________________________|
+''')
+
+name = input("Enter Your Full Name -> ")
+limit = int(input("Enter your range of choice for Guessing the number -> "))
+
+print(f'\n\nWelcome {name} to the Number Guessing Game!')
+
+while 1:
+    num = random.randint(0,limit)
+    lifes = 3
+    while lifes:
+        print()
+        choice = int(input("\n Enter your choice -> "))
+        flag = True
+        if choice == num:
+            print("Hurray! You've won the Game, Let's party.")
+            print('''
+            ________________________________________________
+            |								                |
+            |	    You							            |
+            |	     Won							        |
+            |	      The						            |
+            |	       Game						            |				
+            |_______________________________________________|
+            ''')
+            flag = False
+            break
+        print("Wrong Choice, choose again!")
+        lifes -= 1
+    if flag:
+        print("\n Better luck next time!")
+    
+    repeat = input("\n\n Do you want to play again ? (yes/no) ").lower()
+    if repeat=='yes':
+        pass
+    else:
+        print(f"\n\n Thank you for playing {name}!")
+        break
